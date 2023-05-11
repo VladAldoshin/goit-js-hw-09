0a92ac389b67f5045218b47f4f9eed51e5e99f9e
VladAldoshin <119953324+VladAldoshin@users.noreply.github.com>
2023-05-11 09:29:23 +0000

    Deploying to gh-pages from @ VladAldoshin/goit-js-hw-09@8a2b9150b76ac58147b63fff0dcf95957a12d655 🚀

---
+++ 01-color-switcher.89eb3b6f.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.querySelector("body"),d=null;e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,d=setInterval((function(){a.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.89eb3b6f.js.map
--- 01-color-switcher.a9350120.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");let a=null;t.disabled=!0,e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,a=setInterval((()=>{d=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.addEventListener("click",(()=>{clearInterval(a),e.disabled=!1,t.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.a9350120.js.map
