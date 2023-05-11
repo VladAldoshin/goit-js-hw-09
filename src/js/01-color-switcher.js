801a9c5cc8dff9d6dedf2c628f72f131b28de3a7
VladAldoshin <aldoshinvladislav87@gmail.com>
2023-05-11 14:17:22 +0300

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,7 +2,7 @@ const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
 const bodyRandomColor = document.querySelector('body');
 let timerId = null;
-stopButton.disabled = true;
+
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
