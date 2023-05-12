3da7f5cd96324197e9c6b0044f3afb08d60de3f8
VladAldoshin <aldoshinvladislav87@gmail.com>
2023-05-12 13:04:48 +0300

    change 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,7 +2,7 @@ import Notiflix from 'notiflix';
 const delay = document.querySelector('input[name="delay"]');
 const step = document.querySelector('input[name="step"]');
 const amount = document.querySelector('input[name="amount"]');
-const button = document.querySelector('button [type="submit"]');
+const button = document.querySelector('button[type="submit"]');
 
 
 function createPromise(position, delay) {
